dc9a225fa69e9911642550169f39cd25da8b5776
Lan Zhang <a73486lz@decmtcsaiml01.cs.man.ac.uk>
2024-03-13 15:29:30 +0000

    fixed minor mistakes

--- isabelle/file_handler.py
+++ isabelle/file_handler.py
@@ -2,7 +2,7 @@ def write_to_thy_file(file_path, theory_name, dependency_file, text, statement):
     with open(file_path, 'w') as f1:
         with open(dependency_file, 'r') as f2:
             dependency = f2.read()
-        f1.write(f'theory {theory_name}\n{dependency}\n{text}\n{statement}\nend')
+        f1.write(f'theory {theory_name}\n{dependency}\n{text}\n{statement}')
 
 
 def write_error_to_file(file_path, is_valid, error_lines, error_details, inference_time):
